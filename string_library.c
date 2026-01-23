474d3d27ba1237f2abd9ba445515b86207136e83
Dawa Tamang <elimist63@gmail.com>
2026-01-23 12:00:58 +0545

    improved strcmp function

--- string_library.c
+++ string_library.c
@@ -1,37 +1,32 @@
 #include<stdio.h>
 int my_strlen(const char *);//done
-char* my_strcpy(char* , const char* );//done
+char *my_strcpy(char* , const char* );//done
 int my_strcmp(const char *,const char *);//done
 char* my_strcat(char*,const char *);//done
 int main(){
-    const char* str1 = "kathmandu";
-    const char* str3 = "homecoming";
-    char str4[100]="hello";
-    const char* str5="world";
+    const char* str1 = "app",*str3 = "apple",*str5=" man";
+    char str2[100]="abc",str4[100]="iron";
     int length=my_strlen(str1);
     printf("length= %d\n",length);
-    char str2[20];
-    *my_strcpy(str2,str1);
+    my_strcpy(str2,str1);
     printf("str2: %s\n",str2);
     int back=my_strcmp(str3,str1);
     printf("%d\n",back);
-  my_strcat(str4,str5);
-   printf("%s\n",str4);
+    my_strcat(str4,str5);
+    printf("%s\n",str4);
     return 0;
 }
 
 int my_strlen(const char *str1){
     int count =0;
-    const char *s=str1;
-    while(*s != '\0'){
+    while(*str1 != '\0'){
       count++;
-      s++;
+      str1++;
     }
     return (count);
 }
 char* my_strcpy(char* str2,const char* str1){
 char *cp_str2=str2;
-printf("%s",cp_str2);
 while(*str1!='\0'){
     *str2=*str1;
     str1++;
@@ -41,13 +36,18 @@ while(*str1!='\0'){
 return (cp_str2);
 }
 int my_strcmp(const char *str1,const char *str2){
-    while(*str1!='\0'){
+    while((*str1!='\0') && (*str2!='\0')){
         if(*str1!=*str2)
-        return 0;
+        break;
         str1++;
         str2++;
     }
+    if(*str1==*str2)
+    return 0;
+    else if( *str1 > *str2) 
     return 1;
+    else 
+    return -1;
 }
 
 
@@ -56,8 +56,6 @@ char* cp_str4=str4;
 while(*str4!='\0'){
 str4++;
 }
-*str4=' ';
-str4++;
 while(*str5!='\0'){
     *str4=*str5;
     str4++;
