42ca4ac71db1a4fe8f2a013fc2624170f863d95b
Dawa Tamang <elimist63@gmail.com>
2026-01-24 23:02:34 +0545

    fixed memory allocation

--- student_record.c
+++ student_record.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int n= 0;
 void clear_buffer()
 {
     printf("invalid input!");
@@ -17,13 +16,14 @@ typedef struct
     float gpa;
 } student_record; // struc of size 112 bytes
 
-void add_record(student_record *);
-void display_record(student_record *);
+void add_record(student_record *, int, int);
+void display_record(student_record *, int);
 void search_record(student_record *);
 void delete_record(student_record *);
 
 int main()
 {
+    int total = 0, num = 0;
     student_record *student = NULL;
     int choice;
     while (1)
@@ -41,27 +41,28 @@ int main()
         {
         case 1:
             printf("Number of students: ");
-            if (scanf("%d", &n) != 1)
+            if (scanf("%d", &num) != 1)
             {
                 clear_buffer();
             }
-            else if (n <= 0)
+            else if (num <= 0)
             {
                 printf("Must be greater than 0!\n");
                 continue;
             }
-            student_record *temp_student = (student_record *)realloc(student, sizeof(student_record) * n); // struct student_record now is a user defined data type
+            student_record *temp_student = (student_record *)realloc(student, sizeof(student_record) * total); // struct student_record now is a user defined data type
             if (temp_student == NULL)
             { // check if sufficient memory not allocated
                 printf("memory allocation error!\n");
                 return -1;
             }
             else
-            student=temp_student;
-            add_record(student);
+                student = temp_student;
+            total = num + total;
+            add_record(student, num, total);
             break;
         case 2:
-            display_record(student);
+            display_record(student, total);
             break;
         case 3:
             search_record(student);
@@ -75,32 +76,40 @@ int main()
     }
     return 0;
 }
-void add_record(student_record *student)
+void add_record(student_record *student, int num, int total){
+for (int i = total - num; i < total; i++)
 {
-    for (int i = 0; i < n; i++)
-    {
-        printf("Student %d\n", i + 1);
-        printf("enter your name: ");
-        scanf(" %s", student[i].name);
-        printf("enter your age: ");
-        scanf(" %d", &student[i].age);
-        printf("enter your class: ");
-        scanf(" %d", &student[i].class);
-        printf("enter your roll number: ");
-        scanf(" %d", &student[i].roll_no);
-        printf(" enter your gpa:");
-        scanf(" %f", &student[i].gpa);
+    printf("Student %d\n", i + 1);
+            printf("enter your name: ");
+            scanf(" %s", student[i].name);
+            printf("enter your age: ");
+            scanf(" %d", &student[i].age);
+            printf("enter your class: ");
+            scanf(" %d", &student[i].class);
+            printf("enter your roll number: ");
+            scanf(" %d", &student[i].roll_no);
+            printf(" enter your gpa:");
+            scanf(" %f", &student[i].gpa);
+        }
     }
-}
 
-void display_record(student_record *student) // displys the record
+
+void display_record(student_record *student, int total) // displys the record
 {
+    
+        if (total== 0)
+        {
+            printf("no data\n");
+        }
+        else
+        {
     // student_record new_student=(student_record *)realloc(student,sizeof(student_record)*1);
     printf("s.n|\tName\t|\tage\t|\tclass\t|\trollno\t|\tgpa\t|\n");
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < total; i++)
     {
         printf("%d\t%s\t\t%d\t\t%d\t\t%d\t\t%f\t\n", i + 1, student[i].name, student[i].age, student[i].class, student[i].roll_no, student[i].gpa);
     }
+}
 }
 
 void search_record(student_record *student)
