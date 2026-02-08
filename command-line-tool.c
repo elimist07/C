1b3bf37917a600bd1175af37d8355004fc094e19
elimist07 <elimist63@gmail.com>
2026-02-08 21:52:46 +0545

    command line tool for basic arithmetic completed

--- command-line-tool.c
+++ command-line-tool.c
@@ -1,23 +1,59 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-int add(int,int);
-int main(int argc,char* argv[]){
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int ascii_check(char **);
+int arg_check(int, char **);
+int main(int argc, char *argv[])
+{
+    if (ascii_check(argv))
+    {
+        switch (arg_check(argc, argv))
+        {
+        case 1:
+            printf("Sum = %.2f\n", atof(argv[2]) + atof(argv[3]));
+            break;
+        case 2:
+            printf("Difference= %.2f\n", atof(argv[2]) - atof(argv[3]));
+            break;
+        case 3:
+            printf("Product= %.2f\n", atof(argv[2]) * atof(argv[3]));
+            break;
+        case 4:
+            if (atof(argv[3]) == 0)
+            {
+                printf("Can not divide by zero\n");
+                break;
+            }
+            printf("Quotient= %.2f", atof(argv[2]) / atof(argv[3]));
+            break;
+        default:
+            printf("Insufficient arguments\n");
+        }
+    }
+    else
+        printf("Invalid arguments\n");
+    return 0;
+}
 
-int add_result=strcmp(argv[1],"add");
-   if(add_result==0&&argc==4){
-    int num1=atoi(argv[2]);
-    int num2=atoi(argv[3]);
-    
-   printf("Sum = %d\n",add(num1,num2));
-   
-}
-else{
-    printf("Sufficient arguments needed\n");
-}
-return 0;
-}
+int arg_check(int argc, char *argv[])
+{
 
-int add(int a,int b){
-    return(a+b);
+    if (strcmp(argv[1], "add") == 0 && argc == 4)
+        return 1;
+    if (strcmp(argv[1], "sub") == 0 && argc == 4)
+        return 2;
+    if (strcmp(argv[1], "mult") == 0 && argc == 4)
+        return 3;
+    if (strcmp(argv[1], "div") == 0 && argc == 4)
+        return 4;
+
+    return 0;
+}
+int ascii_check(char *argv[])
+{
+    if (argv[2][0] < 48 || argv[2][0] > 57)
+        return 0;
+    if (argv[3][0] < 48 || argv[3][0] > 57)
+        return 0;
+    return 1;
 }
