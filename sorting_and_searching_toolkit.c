108bc59a3fff1cc2e459abb25ad21f6cb79a40aa
elimist07 <elimist63@gmail.com>
2026-02-20 06:09:01 +0545

    working on binary search...works for ascending int array...

--- sorting_and_searching_toolkit.c
+++ sorting_and_searching_toolkit.c
@@ -6,27 +6,31 @@ int comp_float(void *, void *);
 int comp_string(void *, void *);
 void bubble_sort(void *, int, int, int (*comp)(void *, void *));
 void selection_sort(void *, int, int, int (*comp)(void *, void *));
+void binary_search(void *, int, int, int , int (*comp)(void *, void *));
 
 int main()
 {
-    char array1[][100] = {"Kathmandu", "Delhi", "Berlin","Tokyo","New York","Cape town","Beijing"};
-    int n1 = sizeof(array1) / sizeof(array1[0]);
-    char array2[][100] = {"Kathmandu", "Delhi", "Berlin","Tokyo","New York","Cape town","Beijing"};
-    int n2 = sizeof(array2) / sizeof(array2[0]);
-    // int array[] = {5, 4, 10, 2, 1};
-    //int n = sizeof(array) / sizeof(array[0]);
-    selection_sort(array1, n1, sizeof(array1[0]), comp_string);
-    for (int i = 0; i < n1; i++)
-    {
-        printf("%s\t", array1[i]);
-    }
-    printf("\n");
-    bubble_sort(array2,n2,sizeof(array2[0]),comp_string);
-      for (int i = 0; i < n2; i++)
-    {
-        printf("%s\t", array2[i]);
-    }
-    return 0;
+    // char array1[][100] = {"Kathmandu", "Delhi", "Berlin", "Tokyo", "New York", "Cape town", "Beijing"};
+    // int n1 = sizeof(array1) / sizeof(array1[0]);
+    // char array2[][100] = {"Kathmandu", "Delhi", "Berlin", "Tokyo", "New York", "Cape town", "Beijing"};
+    // int n2 = sizeof(array2) / sizeof(array2[0]);
+    // // int array[] = {5, 4, 10, 2, 1};
+    // // int n = sizeof(array) / sizeof(array[0]);
+    // selection_sort(array1, n1, sizeof(array1[0]), comp_string);
+    // for (int i = 0; i < n1; i++)
+    // {
+    //     printf("%s\t", array1[i]);
+    // }
+    // printf("\n");
+    // bubble_sort(array2, n2, sizeof(array2[0]), comp_string);
+    // for (int i = 0; i < n2; i++)
+    // {
+    //     printf("%s\t", array2[i]);
+    // }
+    // return 0;
+    int ar[] = {12, 13, 114, 17};
+    int n = sizeof(ar) / sizeof(ar[0]);
+    binary_search(ar, n, sizeof(ar[0]),1, comp_int);
 }
 
 void bubble_sort(void *array, int n, int size, int (*comp)(void *, void *))
@@ -51,10 +55,10 @@ void bubble_sort(void *array, int n, int size, int (*comp)(void *, void *))
 void selection_sort(void *array, int n, int size, int (*comp)(void *, void *))
 {
     int no_of_comp = 0, no_of_swaps = 0;
-    for (int i = 0; i < n - 1; ++i)
+    for (int i = 0; i < n - 1; i++)
     {
         int min_index = i;
-        for (int j = i + 1; j < n; ++j)
+        for (int j = i + 1; j < n; j++)
         {
             void *a = (char *)array + min_index * size, *b = (char *)array + j * size;
             no_of_comp++;
@@ -72,7 +76,39 @@ void selection_sort(void *array, int n, int size, int (*comp)(void *, void *))
     }
     printf("No of comparisons=%d\nNo of swaps=%d\n", no_of_comp, no_of_swaps);
 }
-
+void binary_search(void *array, int n, int size, int target, int (*comp)(void *, void *))
+{
+    int low = 0, high = n;
+    int mid_index = low + (high - low) / 2;
+    void *a = (char *)array, *mid = (char *)array + mid_index * size, *b = (char *)array + n * size;
+    if (comp(a, &target) == -1 || comp(b, &target) == 1)
+    {
+        printf("Target not in array");
+        return;
+    }
+    int re;
+    while ((re = comp(&target, mid)) != 0)
+    {
+        if (re == 1)
+        {
+            swap(a, mid, size);
+            low = mid_index;
+            mid_index = low + (high - low) / 2;
+            mid = (char *)array + mid_index * size;
+        }
+        if (re == -1)
+        {
+            swap(b, mid, size);
+            high = mid_index;
+            mid_index = low + (high - low) / 2;
+            mid = (char *)array + mid_index * size;
+        }
+    }
+    if (re == 0)
+        printf("Target found at index %d\n", mid_index);
+    else
+        printf("Target not in array");
+}
 int comp_int(void *a, void *b)
 {
     if (*(int *)a > *(int *)b)
