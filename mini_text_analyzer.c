efe21e1a5e8e1d756a964b78bfb2e5f48f6b2215
elimist07 <elimist63@gmail.com>
2026-02-21 13:15:56 +0545

    working on most frequent characters...

--- mini_text_analyzer.c
+++ mini_text_analyzer.c
@@ -5,15 +5,21 @@ extern int errno;
 int character_count(char **);
 int line_count(char **);
 int word_count(char **);
-int frequent_characters();
+int frequent_characters(char **);
+int isthisspace(unsigned int);
+/*isthisspace() checks  for  white-space  characters.   In  the  "C"  and "POSIX" locales, these are: space, form-feed ('\f'), newline ('\n'), carriage return
+              ('\r'), horizontal tab ('\t'), and vertical tab ('\v').*/
 int main(int argc, char *argv[])
 {
     if (argc == 2)
     {
         fprintf(stdout, "No of Characters: %d\n", character_count(argv));
+        fprintf(stdout, "No of words: %d\n", word_count(argv));
         fprintf(stdout, "No of Lines: %d\n", line_count(argv));
+        fprintf(stdout, "Most frequent character: %c", frequent_characters(argv));
     }
-    else{
+    else
+    {
         fprintf(stdout, "Invalid Syntax");
         return -1;
     }
@@ -22,7 +28,7 @@ int main(int argc, char *argv[])
 
 int character_count(char *argv[])
 {
-    FILE *file = fopen(argv[1], "rb");
+    FILE *file = fopen(argv[1], "r");
     if (file == NULL)
     {
         fprintf(stderr, "%s", strerror(errno));
@@ -39,7 +45,7 @@ int character_count(char *argv[])
 
 int line_count(char *argv[])
 {
-    FILE *file = fopen(argv[1], "rb");
+    FILE *file = fopen(argv[1], "r");
     if (file == NULL)
     {
         fprintf(stderr, "%s", strerror(errno));
@@ -56,16 +62,66 @@ int line_count(char *argv[])
     return lines;
 }
 
-int word_count(char *argv[]){
-FILE *file=fopen(argv[1],"rb");
-if(file==NULL){
-    fprintf(stderr,"%s",strerror(errno));
-    return -1;
+int isthisspace(unsigned int byte)
+{
+    if (byte == ' ' || byte == '\n' || byte == '\f' || byte == '\r' || byte == '\t' || byte == '\v')
+        return 1;
+    return 0;
 }
-int words=0,byte;
-while((byte=fgetc(file)!=EOF)){
-   words++; 
+int word_count(char *argv[])
+{
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "%s", strerror(errno));
+        return -1;
+    }
+    int words = 0, byte, whitespace = 1;
+    while ((byte = fgetc(file)) != EOF)
+    {
+        if (whitespace && !isthisspace(byte))
+        {
+            whitespace = 0;
+            words++;
+        }
+        else if (!whitespace && isthisspace(byte))
+        {
+            whitespace = 1;
+        }
+    }
+    fclose(file);
+    return words;
 }
-fclose(file);
-return words;
+
+int frequent_characters(char *argv[])
+{
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "%s", strerror(errno));
+        return -1;
+    }
+    int characters[256] = {0}, byte;
+    for (int i = 0; i < 255; i++)
+        characters[i] = 0;
+    while ((byte = fgetc(file)) != EOF)
+    {
+        // printf("%c\n",byte);
+        characters[byte] = characters[byte] + 1;
+    }
+    fclose(file);
+
+    for (int i = 0; i < 255 - 1; i++)
+    {
+        for (int j = 0; j < 255 - i; j++)
+        {
+            if (characters[j] > characters[j + 1])
+            {
+                int temp = characters[j];
+                characters[j] = characters[j + 1];
+                characters[j + 1] = temp;
+            }
+        }
+    }
+    return (characters[256]);
 }
